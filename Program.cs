ffe640f74acdbdbf1f03838419714ff97a6d2806
cifulton <cfulton@dunwoody.edu>
2019-11-13 13:14:34 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+//Comment Latest Name and Date
+namespace assignment_four
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Global Declarations
+        }
+
+        //Overloaded method with one argument
+
+
+
+
+        //Overloaded method with two arguments
+    }
+}
